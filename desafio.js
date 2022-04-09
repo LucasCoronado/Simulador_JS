75b31664aa2f79155ea7ca2da5849785c1d8e4be
Lucas Coronado <coronadolucasm@gmail.com>
2022-04-09 00:51:05 -0300

    Condicionales de precio

--- desafio.js
+++ desafio.js
@@ -12,12 +12,34 @@ class Producto {
 }
 
 const articulo = prompt(
-  "Elegi una prenda: \n  Remera $600  \n  Pantalon $550  \n  Campera $350"
+  "Elegi una prenda: \n  Remera   \n  Pantalon   \n  Campera "
 );
+
+
+
+//const precio = parseInt(prompt("Ingrese el precio del producto elegido"));
+
+const precio = 1;
+
+if ((articulo = "Remera") || (articulo = "remera")) {
+    precio = 600
+    alert("El valor de la remera es $600"); 
+} 
+  else if ((articulo = Pantalon) || (articulo = pantalon)){
+    precio = 550
+    alert("El valor del pantalon es $550");  
+}
+  else if ((articulo = Campera) || (articulo = campera)){
+    precio = 350
+    alert("El valor de la campera es $350");
+}
+else {
+  alert("El producto no es valido");
+}
 
 const talle = prompt("Seleccione el talle: \n S  \n M  \n L  \n XL");
 
-const precio = parseInt(prompt("Ingrese el precio del producto elegido"));
+console.log(articulo);
 
 const productoComprado = new Producto(articulo, talle, precio);
 
