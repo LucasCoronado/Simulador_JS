ad953e43a131a2961f3cea35049789d2fbdd51a9
Lucas Coronado <coronadolucasm@gmail.com>
2022-04-09 18:28:40 -0300

    Se agrega array y funcion saludar

--- desafio.js
+++ desafio.js
@@ -1,4 +1,7 @@
-  
+
+let nombre = prompt("Ingrese su nombre");
+saludar();
+
 class Producto {
   constructor(articulo, talle, precio) {
     this.articulo = articulo;
@@ -11,23 +14,29 @@ class Producto {
   }
 }
 
-let precio 
+function saludar() {
+  alert(
+    `Hola ${nombre} bienvenido/a a la tienda`
+  );
+}
+
+let precio
 
 let articulo = prompt(
   "Elegi una prenda: \n  Remera   \n  Pantalon   \n  Campera "
 );
 
 if ((articulo === "Remera") || (articulo === "remera")) {
-    precio = 600
-    alert("El valor de la remera es $600"); 
-} 
-  else if ((articulo === "Pantalon") || (articulo === "pantalon")){
-    precio = 550
-    alert("El valor del pantalon es $550");  
+  precio = 600
+  alert("El valor de la remera es $600");
 }
-  else if ((articulo === "Campera") || (articulo === "campera")){
-    precio = 350
-    alert("El valor de la campera es $350");
+else if ((articulo === "Pantalon") || (articulo === "pantalon")) {
+  precio = 550
+  alert("El valor del pantalon es $550");
+}
+else if ((articulo === "Campera") || (articulo === "campera")) {
+  precio = 350
+  alert("El valor de la campera es $350");
 }
 else {
   alert("El producto no es valido");
@@ -36,11 +45,16 @@ else {
 
 const talle = prompt("Seleccione el talle: \n S  \n M  \n L  \n XL");
 
-console.log(articulo);
-
 const productoComprado = new Producto(articulo, talle, precio);
 
-alert(`Gracias por su compra, usted adquirio un/a  ${
-  productoComprado.articulo
-} en talle ${productoComprado.talle}, el precio final es de 
-${productoComprado.calcularIva()}`);
+const arrayProducto = [];
+
+arrayProducto.push(nombre);
+arrayProducto.push(articulo);
+arrayProducto.push(talle);
+arrayProducto.push(precio);
+console.log(arrayProducto);
+
+alert(`Gracias por su compra, usted adquirio un/a  ${productoComprado.articulo}
+en talle ${productoComprado.talle}, 
+el precio final es de ${productoComprado.calcularIva()}`);
